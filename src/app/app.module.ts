d0899f3d2d2894b5648aff8f26f5e95d4fe9fcb9
Stevenmzv <stevenmezavillegas@hotmail.com>
2021-08-08 22:40:13 -0500

    maquetacion de tarjetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCardsComponent
   ],
   imports: [
     BrowserModule,
